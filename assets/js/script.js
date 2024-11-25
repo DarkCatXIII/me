f6c986e7329c22a2bf5c9ccef18b219f8a5de94a
Tarek iOS <tarek@awfarmarket.com>
2024-11-26 00:24:53 +0200

    update and build education section

--- assets/js/script.js
+++ assets/js/script.js
@@ -446,12 +446,31 @@
                     worksEl = worksEl + work;
                 });
 
-                // educations.array.forEach(education => {
-                    
-                // });
+                educations.forEach(function(element, index, array) {
+                    let education = "<li>" +
+                                "<div class=\"flex items-center justify-between mb-5 md:w-64 md:block md:mb-0\">" +
+                                "<h6 class=\"text-sm font-medium text-black dark:text-white text-opacity-60 md:text-base md:text-opacity-100\">"
+                                element.name +
+                                "</h6>" +
+                                "<p class=\"text-[13px] md:text-sm text-theme\">" +
+                                element.date +
+                                "</p>" +
+                                "</div>" +
+                                "<div class=\"md:flex-1 md:pl-16 relative md:before:content-[''] md:before:absolute md:before:-left-1 md:before:top-3 md:before:w-2 md:before:h-2 md:before:bg-theme md:before:rounded-full md:before:shadow-dots_glow\">" +
+                                "<h4 class=\"text-xl xl:text-2xl font-medium xl:font-medium leading-7 text-black dark:text-white mb-2.5\">" +
+                                element.tite + 
+                                "</h4>" +
+                                "<p>" +
+                                element.todo +
+                                "</p>" +
+                                "</div>" +
+                                "</li>";
+
+                    educationsEl = educationsEl + education;
+                });
                 
                 workDiv.innerHTML = worksEl;
-                // educationDiv.innerHTML = educationsEl;
+                educationDiv.innerHTML = educationsEl;
             },
             error: function (e) {
                 let err = e.error
